41833241ba19ec01b53e19ebfdc6e62ab428b0ea
Patrycja Pieńkowska <129986721+patrycjap03@users.noreply.github.com>
2025-02-27 23:21:57 +0100

    Add files via upload

---
+++ sokoban.cpp
@@ -0,0 +1,58 @@
+﻿ //projekt pp.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+
+
+#include <iostream>
+using namespace std;
+
+
+void ekran_powitalny()
+{
+	cout << "           gra sokoban      " << endl;
+	cout << "autor: patrycja pienkowska s193452" << endl;
+	cout << "              menu:         " << endl;
+	cout << "          rozpocznij gre     " << endl;
+	cout << "             wyjscie      " << endl;
+
+
+}
+
+char mapa[10][25] =
+{
+ "########################",
+ "# x    ########        #",
+ "#                      #",
+ "#                      #",
+ "#         #######      #",
+ "#                      #",
+ "#                      #",
+ "#                      #",
+ "###########   x        #",
+ "########################"
+};
+
+void pokaz_mape()
+{
+
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%s\n", mapa[i]);
+	}
+}
+
+int main()
+{
+	ekran_powitalny();
+
+
+	
+
+
+
+
+	
+
+
+
+	return 0;
+}
+
